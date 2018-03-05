a28d1f702adf040d902c5d7d3f556bf076cfd4b2
hikmat <mallahikma45@gmail.com>
2018-03-05 10:43:11 +0545

    project finalised

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    url(r'^$', AdminLoginView.as_view(), name='adminlogin'),
-    url(r'^home/$',
+    url(r'^login/$', AdminLoginView.as_view(), name='adminlogin'),
+    url(r'^$',
         AdminHomeView.as_view(), name='adminhome'),
     url(r'^admin-dash/$',
         AdminDashView.as_view(), name='admindash'),
@@ -14,16 +14,25 @@ urlpatterns = [
         AdminCreateView.as_view(), name='admincreate'),
     url(r'^admin-dash/read/$',
         AdminReadView.as_view(), name='adminread'),
-    url(r'^admin-dash/delete/(?P<pk>\d+)/$',
+    url(r'^admin-dash/student/delete/(?P<pk>\d+)/$',
         AdminDeleteView.as_view(), name='admindelete'),
-    url(r'^admin-dash/update/(?P<pk>\d+)/$',
+    url(r'^admin-dash/student/update/(?P<pk>\d+)/$',
         AdminUpdateView.as_view(), name='adminupdate'),
     url(r'^admin-dash/issues/$',
         AdminIssueListView.as_view(), name='adminstudentlist'),
+    # book url
     url(r'^admin-dash/book/$',
         AdminBookView.as_view(), name="adminbooklist"),
     url(r'^admin-dash/bookadd/$',
         AdminAddBookView.as_view(), name='adminaddbook'),
+    url(r'^admin-dash/book/delete/(?P<pk>\d+)/$',
+        AdminBookDeleteView.as_view(), name="adminbookdelete"),
+    url(r'^admin-dash/book/update/(?P<pk>\d+)/$',
+        AdminBookUpdateView.as_view(), name="adminbookupdate"),
+
+    # book isssue url
+    url(r'^admin-dash/issues/$',
+        AdminIssueListView.as_view(), name='adminstudentlist'),
     url('^admin-dash/issue/$',
         AdminIssueCreateView.as_view(), name="adminissue"),
     url('^admin-dash/issue-read/$',
--- libraryapp/views.py
+++ libraryapp/views.py
@@ -7,52 +7,71 @@ from django.http import HttpResponseRedirect
 
 
 class AdminDashView(LoginRequiredMixin, TemplateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/admindash.html'
 
 
 class AdminHomeView(LoginRequiredMixin, TemplateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/adminhome.html'
 
 
 class AdminCreateView(LoginRequiredMixin, CreateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/admincreate.html'
     form_class = StudentForm
     success_url = '/admin-dash/read/'
 
+# Book views are defined here
+
 
 class AdminBookView(LoginRequiredMixin, ListView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'book/adminbooklist.html'
     model = Book
     context_object_name = 'books'
 
 
 class AdminAddBookView(LoginRequiredMixin, CreateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'book/adminaddbook.html'
     form_class = BookForm
     success_url = '/admin-dash/book'
 
 
+class AdminBookDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = '/login/'
+    template_name = 'book/adminbookdelete.html'
+    queryset = Book.objects.all()
+    model = Book
+    success_url = '/admin-dash/book/'
+
+
+class AdminBookUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    template_name = 'book/adminbookupdate.html'
+    queryset = Book.objects.all()
+    modal = Book
+    form_class = BookForm
+    success_url = '/admin-dash/book/'
+
+
 class AdminReadView(LoginRequiredMixin, ListView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/adminread.html'
     model = Student
     context_object_name = 'students'
 
 
 class AdminDeleteView(LoginRequiredMixin, DeleteView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/admindelete.html'
     model = Student
     success_url = '/admin-dash/read/'
 
 
 class AdminUpdateView(LoginRequiredMixin, UpdateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'student/adminupdate.html'
     model = Student
     form_class = StudentForm
@@ -62,7 +81,7 @@ class AdminUpdateView(LoginRequiredMixin, UpdateView):
 class AdminLoginView(FormView):
     template_name = 'student/adminlogin.html'
     form_class = AdminLoginForm
-    success_url = '/home/'
+    success_url = '/'
 
     def form_valid(self, form):
         u_name = form.cleaned_data['user_name']
@@ -76,22 +95,22 @@ class AdminLoginView(FormView):
 
 
 class AdminLogoutView(LoginRequiredMixin, View):
-    login_url = '/'
+    login_url = '/login/'
 
     def get(self, request):
         logout(request)
-        HttpResponseRedirect('/adminlogin/')
+        return HttpResponseRedirect('/login/')
 
 
 class AdminIssueCreateView(LoginRequiredMixin, CreateView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'issue/adminissue.html'
     form_class = IssueForm
     success_url = '/admin-dash/issue-read/'
 
 
 class AdminIssueListView(LoginRequiredMixin, ListView):
-    login_url = '/'
+    login_url = '/login/'
     template_name = 'issue/adminissuelist.html'
     queryset = Issue.objects.all()
     context_object_name = 'issues'
